7bc631fa7a0c45ada6abf1b781357edb3ba6f64f
ryan monaghan <rmonaghan90@gmail.com>
2017-11-02 21:33:27 -0700

    11.2.17

---
+++ ConsoleCalculator/Calculate.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator
+{
+    public static class Calculate
+    {
+        #region Adding
+        public static int Add(int[] x)
+        {
+            int y = 0;
+            foreach (int i in x)
+            {
+                y += i;
+            }
+            return y;
+        }
+
+        public static long Add(long[] x)
+        {
+            long y = 0;
+            foreach (int i in x)
+            {
+                y += i;
+            }
+            return y;
+        }
+
+        public static double Add(double[] x)
+        {
+            double y = 0;
+            foreach (int i in x)
+            {
+                y += i;
+            }
+            return y;
+        }
+
+        #endregion
+        #region Subtracting
+        public static int Subtract(int[] x)
+        {
+            int y = x[0];
+            foreach (int i in x.Skip(1))
+            {
+                y -= i;
+            }
+            return y;
+        }
+
+        public static long Subtract(long[] x)
+        {
+            long y = x[0];
+            foreach (long i in x.Skip(1))
+            {
+                y -= i;
+            }
+            return y;
+        }
+
+        public static double Subtract(double[] x)
+        {
+            double y = x[0];
+            foreach (double i in x.Skip(1))
+            {
+                y -= i;
+            }
+            return y;
+        }
+
+        #endregion
+        #region Multiply
+        public static int Multiply(int[] x)
+        {
+            int y = 0;
+            foreach (int i in x)
+            {
+                y += i;
+            }
+            return y;
+        }
+        public static long Multiply(long[] x)
+        {
+            long y = 0;
+            foreach (int i in x)
+            {
+                y += i;
+            }
+            return y;
+        }
+        public static double Multiply(double[] x)
+        {
+            double y = 0;
+            foreach (int i in x)
+            {
+                y = y / i;
+            }
+            return y;
+        }
+        #endregion
+        #region Divide
+        public static int Divide(int[] x)
+        {
+            int y = x[0];
+            foreach (int i in x.Skip(1))
+            {
+               y = y / i;
+            }
+            return y;
+        }
+
+        public static long Divide(long[] x)
+        {
+            long y = x[0];
+            foreach (long i in x.Skip(1))
+            {
+                y = y / i;
+            }
+            return y;
+        }
+
+        public static double Divide(double[] x)
+        {
+            double y = x[0];
+            foreach (double i in x.Skip(1))
+            {
+                y -= i;
+            }
+            return y;
+        }
+    }
+#endregion
+}
--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -10,6 +10,55 @@ namespace ConsoleCalculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter numbers you would like to caluclate.");
+            Console.WriteLine("Seperate the numbers by spaces.");
+            string input = (Console.ReadLine());
+            string[] parsedInput = input.Split(' ');
+            foreach (string x in parsedInput)
+            {
+                try
+                {
+                    int number = Int32.Parse(x);
+                    Console.Write("{0} ", number);
+                }
+                catch(System.FormatException ex1)
+                {
+                    try
+                    {
+                        long number = long.Parse(x);
+                        Console.Write("{0} ", number);
+                    }
+                    catch (System.FormatException ex2)
+                    {
+                        double number = double.Parse(x);
+                        Console.Write("{0} ", number);
+                    }
+                    catch (SystemException wtf)
+                    {
+                        Console.WriteLine("Invalid Entry, what are you trying to do to me?");
+                    }
+                }
+                catch (System.OverflowException exOV)
+                {
+                    try
+                    {
+                        long number = long.Parse(x);
+                        Console.Write("{0} ", number);
+                    }
+                    catch (System.FormatException exOV1)
+                    {
+                        double number = double.Parse(x);
+                        Console.Write("{0} ", number);
+                    }
+                    catch(SystemException wtf)
+                    {
+                        Console.WriteLine("Invalid Entry, what are you trying to do to me?");
+                    }
+                }
+
+            }
+            Console.Read();
+
         }
     }
 }
